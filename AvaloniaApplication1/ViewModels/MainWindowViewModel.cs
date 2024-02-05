8d869d97c41e0223e454013d00905c08c455dfd7
ArrayKat <kolinichenko.05@mail.ru>
2024-02-05 04:05:22 +0300

    create project

---
+++ AvaloniaApplication1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using ReactiveUI;
+
+namespace AvaloniaApplication1.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase 
+    {
+#pragma warning disable CA1822 // Mark members as static
+        private string greeting="pORGI ZBIHAL";
+        public void Command()
+        {
+            Greeting = "new message";  
+            this.RaisePropertyChanged(nameof(Greeting));
+        }
+        public string Greeting { get => greeting; set => greeting = value; }
+#pragma warning restore CA1822 // Mark members as static
+    }
+}
